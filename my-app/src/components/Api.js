ed03606074693afa4d0df04cd5ca55cbe0c7a965
Abhi Vinnakota <abhi.vinnakota5@gmail.com>
2022-01-05 22:17:02 -0600

    finished api call function for nfts

--- my-app/src/components/Api.js
+++ my-app/src/components/Api.js
@@ -2,35 +2,53 @@ import React from "react";
 
 
 async function getOpenseaItems() {
+    if (!window.userAddress) { return }
     const osContainer = document.getElementById('openseaItems')
 
-    const items = await fetch(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50`)
-        .then((res) => res.json())
-        .then((res) => {
-            return res.assets
-        })
-        .catch((e) => {
-            console.error(e)
-            console.error('Could not connect to OpenSea')
-            return null
-        })
+    const items = await fetch(`https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20`)
+      .then((res) => res.json())
+      .then((res) => {
+        return res.assets
+      })
+      .catch((e) => {
+        console.error(e)
+        console.error('Could not talk to OpenSea')
+        return null
+      })
+
     if (items.length === 0) { return }
 
-    items.forEach(nft => {
-        const { name, image_url, description, permalink } = nft 
-        const newElement = document.createElement('div')
-        newElement.innerHTML = `
-            <h1>'${name}'</h1>
-            <h1>'${permalink}</h1>
-        `
-        osContainer.appendChild(newElement);
+    items.forEach((nft) => {
+      const { name, image_url, description, permalink } = nft
+
+      const newElement = document.createElement('div')
+      newElement.innerHTML = `
+        <!-- Opensea listing item-->
+        <a href='${permalink}' target="_blank">
+          <div class='flex flex-col'>
+            <img
+              src='${image_url}'
+              class='w-full rounded-lg' />
+            <div class='flex-col w-full space-y-1'>
+              <p class='text-gray-800 text-lg'>${name}</p>
+              <p class='text-gray-500 text-xs word-wrap'>${description ?? ''}</p>
+            </div>
+          </div>
+        </a>
+        <!-- End Opensea listing item-->
+      `
+
+      osContainer.appendChild(newElement)
     })
-}
+  }
 class Api extends React.Component {
+    getOpenseaItems = () => {
+        console.log('click happened');
+    }
     render() {
         return (
             <div id = "api">
-                <button onclick = "getOpenseaItems()">Get Opensea Items</button>
+                <button onClick = {this.getOpenseaItems}>Get Opensea Items</button>
                 <div id = 'openseaItems'>
                 
                 </div>
